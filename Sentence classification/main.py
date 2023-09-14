143d14c048de7b1b6f0287f4e249f0dc79848cc2
gangjoohyeng <bles@kakao.com>
2023-09-14 22:19:07 +0900

    style: remove comment

--- Sentence classification/main.py
+++ Sentence classification/main.py
@@ -3,23 +3,6 @@ import lightning.pytorch as pl
 from dataset import YNATDataModule
 from model import YNATModel
 
-# klue_tokenizer = AutoTokenizer.from_pretrained('klue/bert-base')
-# kykim_tokenizer = AutoTokenizer.from_pretrained('kykim/bert-kor-base')
-# snunlp_tokenizer = AutoTokenizer.from_pretrained('snunlp/KR-BERT-char16424')
-
-# dataset = load_dataset('klue', 'ynat')
-
-# train_test_dataset = dataset['train']
-# valid_dataset = dataset['validation']
-
-# train_test = train_test_dataset.train_test_split(train_size=0.9, shuffle=False)
-# train_dataset, test_dataset = train_test['train'], train_test['test']
-
-# klue = BertForSequenceClassification.from_pretrained('klue/bert-base', num_labels=7)
-# kykim = BertForSequenceClassification.from_pretrained('kykim/bert-kor-base', num_labels=7)
-# snunlp = BertForSequenceClassification.from_pretrained('snunlp/KR-BERT-char16424', num_labels=7)
-
-
 def run():
     klue_datamodule = YNATDataModule(model_name='klue/bert-base')
     klue_model = YNATModel(model_name='klue/bert-base')
